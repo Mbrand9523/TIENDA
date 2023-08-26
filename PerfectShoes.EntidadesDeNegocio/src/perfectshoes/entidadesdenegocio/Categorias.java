6f21377dd80c617a74cf965c39c321bef3c07347
alexandragalicia09 <alexandragalicia09@outlook.com>
2023-08-26 06:39:14 -0600

    Se crearon las DAL Categorias y Productos

--- PerfectShoes.EntidadesDeNegocio/src/perfectshoes/entidadesdenegocio/Categorias.java
+++ PerfectShoes.EntidadesDeNegocio/src/perfectshoes/entidadesdenegocio/Categorias.java
@@ -10,62 +10,6 @@ package perfectshoes.entidadesdenegocio;
  */
 
 public class Categorias {
-    private int id;
-    private int idProductos;
-    private String nombrescategorias;
-    private String productos;  
-    private int top_aux;
-
-    public Categorias() {
-    }
-
-    public Categorias(int id, int idProductos, String nombrescategorias, String productos, int top_aux) {
-        this.id = id;
-        this.idProductos = idProductos;
-        this.nombrescategorias = nombrescategorias;
-        this.productos = productos;
-        this.top_aux = top_aux;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getIdProductos() {
-        return idProductos;
-    }
-
-    public void setIdProductos(int idProductos) {
-        this.idProductos = idProductos;
-    }
-
-    public String getNombrescategorias() {
-        return nombrescategorias;
-    }
-
-    public void setNombrescategorias(String nombrescategorias) {
-        this.nombrescategorias = nombrescategorias;
-    }
-
-    public String getProductos() {
-        return productos;
-    }
-
-    public void setProductos(String productos) {
-        this.productos = productos;
-    }
-
-    public int getTop_aux() {
-        return top_aux;
-    }
-
-    public void setTop_aux(int top_aux) {
-        this.top_aux = top_aux;
-    }
     
     
 }
